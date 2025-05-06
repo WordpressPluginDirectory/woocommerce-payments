ca92ae407a5e540249fcc894007b351166ea04a5
Justin Gardner <justin@rhynorater.com>
2025-05-06 06:02:30 -0500

    9.3.0

--- woocommerce-payments/includes/express-checkout/class-wc-payments-express-checkout-button-handler.php
+++ woocommerce-payments/includes/express-checkout/class-wc-payments-express-checkout-button-handler.php
@@ -250,6 +250,7 @@ class WC_Payments_Express_Checkout_Button_Handler {
 				// Defaults to 'required' to match how core initializes this option.
 				'needs_payer_phone'          => 'required' === get_option( 'woocommerce_checkout_phone_field', 'required' ),
 				'allowed_shipping_countries' => array_keys( WC()->countries->get_shipping_countries() ?? [] ),
+				'display_prices_with_tax'    => 'incl' === get_option( 'woocommerce_tax_display_cart' ),
 			],
 			'button'             => $this->get_button_settings(),
 			'login_confirmation' => $this->get_login_confirmation_settings(),
@@ -307,6 +308,7 @@ class WC_Payments_Express_Checkout_Button_Handler {
 		}
 
 		wp_localize_script( 'WCPAY_EXPRESS_CHECKOUT_ECE', 'wcpayExpressCheckoutParams', $express_checkout_params );
+		wp_localize_script( 'WCPAY_BLOCKS_CHECKOUT', 'wcpayExpressCheckoutParams', $express_checkout_params );
 
 		wp_set_script_translations( 'WCPAY_EXPRESS_CHECKOUT_ECE', 'woocommerce-payments' );
 
@@ -328,7 +330,7 @@ class WC_Payments_Express_Checkout_Button_Handler {
 			return;
 		}
 		?>
-		<div id="wcpay-express-checkout-element" style="display: none;"></div>
+		<div id="wcpay-express-checkout-element"></div>
 		<?php
 	}
 
